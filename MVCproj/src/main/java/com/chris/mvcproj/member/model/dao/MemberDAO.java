fa014e2d7740b77bf20fcd459ea14121f75e7272
KyungRyeol Yoon <kr.yoon1101@gmail.com>
2019-04-23 17:23:39 +0900

    Add files via upload

---
+++ MVCproj/src/main/java/com/chris/mvcproj/member/model/dao/MemberDAO.java
@@ -0,0 +1,20 @@
+package com.chris.mvcproj.member.model.dao;
+
+import java.util.List;
+
+import com.chris.mvcproj.member.model.dto.MemberVO;
+ 
+public interface MemberDAO {
+    // 회원 목록 
+    public List<MemberVO> memberList();
+    // 회원 입력
+    public void insertMember(MemberVO vo);
+    // 회원 정보 상세보기
+    public MemberVO viewMember(String userId);
+    // 회원삭제
+    public void deleteMember(String userId);
+    // 회원정보 수정
+    public void updateMember(MemberVO vo);
+    // 회원 정보 수정 및 삭제를 위한 비밀번호 체크
+    public boolean checkPw(String userId, String userPw);
+}
